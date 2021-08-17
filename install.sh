cd77ba032f7833bf752468ce4b457f114c978d6f
Christopher Soehnlein <admin@islanddog.ky>
2021-08-17 16:06:11 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,9 @@
 sudo apt-get install -y arandr flameshot arc-theme feh i3blocks i3status i3 i3-wm lxappearance python3-pip rofi unclutter cargo compton papirus-icon-theme imagemagick
 sudo apt-get install -y libxcb-shape0-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev autoconf meson
 sudo apt-get install -y libxcb-render-util0-dev libxcb-shape0-dev libxcb-xfixes0-dev 
-
+sudo apt-get install seclists curl enum4linux gobuster nbtscan nikto nmap oscanner smbclient smbmap smtp-user-enum snmp sslscan tnscmd10g whatweb
+cargo install rustscan
+cargo install feroxbuster
 git clone https://github.com/ryanoasis/nerd-fonts.git
 cd nerd-fonts
 ./install.sh
