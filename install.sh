256d25331a0dbfd8dad849ad96ed2d75346c2220
Christopher Soehnlein <admin@islanddog.ky>
2021-08-19 13:55:13 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 sudo apt-get install -y arandr flameshot arc-theme feh i3blocks i3status i3 i3-wm lxappearance python3-pip rofi unclutter cargo compton papirus-icon-theme imagemagick
 sudo apt-get install -y libxcb-shape0-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev autoconf meson
 sudo apt-get install -y libxcb-render-util0-dev libxcb-shape0-dev libxcb-xfixes0-dev 
-sudo apt-get install seclists curl enum4linux gobuster nbtscan nikto nmap oscanner smbclient smbmap smtp-user-enum snmp sslscan tnscmd10g whatweb
+sudo apt-get install -y onedrive enum4linux nbtscan nikto nmap oscanner smbclient smbmap smtp-user-enum snmp sslscan whatweb
 cargo install rustscan
 cargo install feroxbuster
 git clone https://github.com/ryanoasis/nerd-fonts.git
@@ -35,7 +35,9 @@ cp .config/compton/compton.conf ~/.config/compton/compton.conf
 cp .config/rofi/config ~/.config/rofi/config
 cp .fehbg ~/.fehbg
 cp .config/i3/clipboard_fix.sh ~/.config/i3/clipboard_fix.sh
+sudo mv .bashrc bashrc.bak && sudo mv .zshrc zshrc.bak
+sudo cp .bashrc .zshrc ~/.
+sudo mv vpnbash.sh vpnserver.sh /opt/
 
 echo "Done! Grab some wallpaper and run pywal -i filename to set your color scheme. To have the wallpaper set on every boot edit ~.fehbg"
-echo ".bash/.zshrc included in repo as well. Overwrite or merge to get PwnBox layout. Need to copy '.sh' files over to /opt as well."
 echo "After reboot: Select i3 on login, run lxappearance and select arc-dark"
