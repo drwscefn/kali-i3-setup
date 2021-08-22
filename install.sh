c6921b1321b12c092f0b36b95f2e0f9798a9bbc4
Christopher Soehnlein <admin@islanddog.ky>
2021-08-22 11:17:55 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,21 +8,21 @@ cargo install rustscan
 cargo install feroxbuster
 
 cd /opt/
-git clone https://github.com/carlospolop/privilege-escalation-awesome-scripts-suite
-git clone https://github.com/rebootuser/LinEnum
-git clone https://github.com/M4ximuss/Powerless
-git clone https://github.com/r3motecontrol/Ghostpack-CompiledBinaries
-git clone https://github.com/ivan-sincek/php-reverse-shell.git
-git clone https://github.com/samratashok/nishang.git
-git clone https://github.com/danielmiessler/SecLists.git
-git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git
+sudo git clone https://github.com/carlospolop/privilege-escalation-awesome-scripts-suite
+sudo git clone https://github.com/rebootuser/LinEnum
+sudo git clone https://github.com/M4ximuss/Powerless
+sudo git clone https://github.com/r3motecontrol/Ghostpack-CompiledBinaries
+sudo git clone https://github.com/ivan-sincek/php-reverse-shell.git
+sudo git clone https://github.com/samratashok/nishang.git
+sudo git clone https://github.com/danielmiessler/SecLists.git
+sudo git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git
 sudo mv zsh-autocomplete /usr/share/
-git clone https://github.com/jwilm/alacritty
+sudo git clone https://github.com/jwilm/alacritty
 cd alacritty
 cargo build --release && sudo cp target/release/alacritty /usr/local/bin
 cd ..
 
-git clone https://www.github.com/Airblader/i3 i3-gaps
+sudo git clone https://www.github.com/Airblader/i3 i3-gaps
 cd i3-gaps && mkdir -p build && cd build && meson ..
 ninja
 sudo ninja install
@@ -36,12 +36,12 @@ mkdir -p ~/.config/compton
 mkdir -p ~/.config/rofi
 mkdir -p ~/.config/alacritty
 mkdir -p ~/.wallpaper
-cp .config/i3/config ~/.config/i3/config
-cp .config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
-cp .config/i3/i3blocks.conf ~/.config/i3/i3blocks.conf
-cp .config/compton/compton.conf ~/.config/compton/compton.conf
-cp .config/rofi/config ~/.config/rofi/config
-cp .fehbg ~/.fehbg
-cp .config/i3/clipboard_fix.sh ~/.config/i3/clipboard_fix.sh
+sudo cp .config/i3/config ~/.config/i3/config
+sudo cp .config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+sudo cp .config/i3/i3blocks.conf ~/.config/i3/i3blocks.conf
+sudo cp .config/compton/compton.conf ~/.config/compton/compton.conf
+sudo cp .config/rofi/config ~/.config/rofi/config
+sudo cp .fehbg ~/.fehbg
+sudo cp .config/i3/clipboard_fix.sh ~/.config/i3/clipboard_fix.sh
 sudo mv vpnbash.sh vpnserver.sh /opt/
 sudo mv htb.jpg htb2.jpg ~/.wallpaper
