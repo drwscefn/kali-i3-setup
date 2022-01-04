cada92ba4100c91e14290a9c1e1e73c1e1639b31
Christopher Soehnlein <admin@islanddog.ky>
2022-01-04 15:51:08 -0500

    2022 Update
    Added additional tools and merged the repo instructions

--- install.sh
+++ install.sh
@@ -2,9 +2,28 @@
 
 echo -e "\e[41mPwnBox Install\e[0m"
 echo -e "\e[41mBased off xct/clean and theGuildHall/pwnbox\e[0m"
-echo -e "\e[41mUpdated 09.06.2021 \e[24m"
+echo -e "\e[41mUpdated 01.04.2022 \e[24m"
 echo -e "\e[41mBy:Christopher Soehnlein | https://IslandDog.ky \e[0m"
 read -s -n 1 -p "Press ANY key to continue."
+
+#KaliLinux New Installs
+read -s -n 1 -p "Nano is about to launch, copy the following before proceeding with the install"
+sudo nano /etc/apt/sources.list
+echo -e "deb http://http.kali.org/kali kali main contrib non-free"
+echo -e "deb-src http://http.kali.org/kali kali main contrib non-free"
+echo -e "deb http://security.kali.org/kali-security kali/updates main contrib non-free"
+echo -e "deb-src http://security.kali.org/kali-security kali/updates main contrib non-free"
+read -s -n 1 -p "Confirm this has been copied before proceeding with the install."
+
+sudo apt-get install build-essential
+sudo apt-get install build-essential checkinstall
+sudo apt-get install autoconf automake autotools-dev m4
+sudo apt-get install libx11-dev
+sudo apt-get install freeglut3-dev
+sudo apt-get install -y arandr flameshot arc-theme feh i3blocks i3status i3 i3-wm lxappearance python3-pip rofi unclutter cargo compton papirus-icon-theme imagemagick
+sudo apt-get install -y libxcb-shape0-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev autoconf meson
+sudo apt-get install -y libxcb-render-util0-dev libxcb-shape0-dev libxcb-xfixes0-dev 
+sudo apt-get install -y gobuster onedrive enum4linux nbtscan nikto nmap oscanner smbclient smbmap smtp-user-enum snmp sslscan whatweb
 
 mkdir -p ~/.config/i3
 mkdir -p ~/.config/compton
@@ -22,10 +41,6 @@ sudo mv vpnbash.sh vpnserver.sh /opt/
 sudo chmod +x /opt/vpn*.sh
 sudo mv htb.jpg htb2.jpg ~/.wallpaper
 
-sudo apt-get install -y arandr flameshot arc-theme feh i3blocks i3status i3 i3-wm lxappearance python3-pip rofi unclutter cargo compton papirus-icon-theme imagemagick
-sudo apt-get install -y libxcb-shape0-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev autoconf meson
-sudo apt-get install -y libxcb-render-util0-dev libxcb-shape0-dev libxcb-xfixes0-dev 
-sudo apt-get install -y gobuster onedrive enum4linux nbtscan nikto nmap oscanner smbclient smbmap smtp-user-enum snmp sslscan whatweb
 curl https://sh.rustup.rs -sSf | sh
 cargo install rustscan feroxbuster
 echo -e "\e[41mNOTICE\e[0m"
@@ -57,7 +72,7 @@ pip3 install pywal
 
 echo -e "\e[41mNOTICE\e[0m"
 echo "Now installing Nerd Fonts. This process can take 20-25minutes."
-read -s -n 1 -p "Press ANY key to continue."
+read -s -n 1 -p "Press ANY key to continue or Ctrl+C to Cancel and run this install at a later time."
 sudo git clone https://github.com/ryanoasis/nerd-fonts.git
 cd nerd-fonts
 sudo chmod +x install.sh
