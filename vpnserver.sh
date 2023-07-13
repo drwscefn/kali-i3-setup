e0313071f0d4b49c152a096da3043f646c25740d
Christopher Soehnlein <admin@islanddog.ky>
2023-07-13 14:19:09 -0500

    Update vpnserver.sh
    Requires a folder under Documents called htb. Change Line 3 if you want to select your own folder.

--- vpnserver.sh
+++ vpnserver.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 htbip=$(ip addr | grep tun0 | grep inet | grep 10. | tr -s " " | cut -d " " -f 3 | cut -d "/" -f 1)
-lab=$(cat ~/htb/lab_*.ovpn | grep "remote " | cut -d " " -f 2 | cut -d "." -f 1 | cut -d "-" -f 2-)
-ra=$(cat ~/htb/release_arena_*.ovpn | grep "remote " | cut -d " " -f 2 | cut -d "." -f 1 | cut -d "-" -f 2-)
+myip=$(cat ~/Documents/htb/*.ovpn | grep "remote " | cut -d " " -f 2 | cut -d "." -f 1 | cut -d "-" -f 2-)
 
 if [[ $htbip == *"10."* ]]
 then
-   echo "$lab" || echo "$ra"
+   echo "$myip"
 else
    echo ""
 fi
